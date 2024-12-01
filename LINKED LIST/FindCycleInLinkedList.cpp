ff2a605358aa5ab10cb1042e8cf3be375f75f709
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2024-12-01 17:44:26 +0530

    Add files via upload

---
+++ LINKED LIST/FindCycleInLinkedList.cpp
@@ -0,0 +1,5 @@
+//given the head of th elinked list, find if there is a cycle in it
+
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> -> 3
+// here 7 is pointing back to 3 so theres a cycle.
+// 3 4 5 6 7
---
+++ LINKED LIST/FindMiddleEleOfLL.cpp
@@ -0,0 +1,1 @@
+//given the head of the linked list, find the middle of the linked list.
