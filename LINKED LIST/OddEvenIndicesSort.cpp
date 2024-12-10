fed22daa4188c8fe2aea61c458d785d86a475170
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2024-12-10 23:09:45 +0530

    Add files via upload

---
+++ LINKED LIST/OddEvenIndicesSort.cpp
@@ -0,0 +1,4 @@
+// given the head of the linked list, arrange the odd indices nodes together and even indices nodes together, then return the head of the new linked list.
+// 0 - indexing
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8
+// 1 -> 3 -> 5 -> 7 -> 2 -> 4 -> 6 -> 8
